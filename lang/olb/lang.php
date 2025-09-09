42e2d3f32c0664a98c7ed4ed06c988308dbe9864
karuchkar <80837442+karkar47@users.noreply.github.com>
2025-09-09 23:06:24 +0300

    олбанскей йазыг

---
+++ lang/olb/lang.php
@@ -0,0 +1,136 @@
+<?php 
+    // Side bar
+
+    $lang['home'] = 'Дамой';
+    $lang['feed'] = 'Новасте';
+    $lang['search'] = 'Поеск';
+    $lang['music'] = 'Музычко';
+    $lang['settings'] = 'Настройке';
+    $lang['admin_panel'] = 'Панель мандатра';
+    $lang['login'] = 'Я забыл подписацца, медвед'; // В оригинале звучит так: Я забыл подписацца, асёл
+    $lang['reg'] = 'Регистрация медведей';
+
+    // Header
+
+    $lang['menu'] = 'Менью';
+    $lang['logout'] = 'Выйти';
+    $lang['change_avatar'] = 'изменить юзерпик';
+
+    // Main page
+
+    $lang['index_text1'] = "Превед в наш {$sitename}!!!";
+    $lang['index_text2'] = '<p style="text-align: center;"><img src="https://s00.yaplakal.com/pics/pics_original/3/8/1/14586183.jpg" width="300" height="300"><p style="text-align: center;">Новая гениальная социальная сеть, ловит даже на парковке!</p>';
+    $lang['terms_text'] = '
+            <h3>Справка по Олбанскему языку:</h3>
+        <p style="text-align: center;"><img src="https://photobooth.cdn.sports.ru/preset/message/6/c0/0f4badf0844b8a2c38d362a16a8b9.png?f=webp&q=90&s=2x&w=730" width="400" height="400"></p>
+        <p>
+            Олбанскей, Язык падонков (он же олбанскей йезыг) — метафорический аллегорический язык выражения мыслей в неудобочитаемой форме. Фактически основной концепцией этого т. н. йезыка является альтернативное правописание, при котором слово пишется максимально непохожим на словарное написание при сохранении фонетического образа.<br><br>
+            Луркать больше про Олбанскей:
+        </p>
+            <ul>
+                <li><a href="https://ru.wikipedia.org/wiki/%D0%96%D0%B0%D1%80%D0%B3%D0%BE%D0%BD_%D0%BF%D0%B0%D0%B4%D0%BE%D0%BD%D0%BA%D0%BE%D0%B2">Страница в Википедии</a></li>
+                <li><a href="https://neolurk.org/wiki/%D0%AF%D0%B7%D1%8B%D0%BA_%D0%BF%D0%B0%D0%B4%D0%BE%D0%BD%D0%BA%D0%BE%D0%B2">Страница на Неолурке</a></li>
+                <li><a href="https://lurkmore.media/%D0%AF%D0%B7%D1%8B%D0%BA_%D0%BF%D0%B0%D0%B4%D0%BE%D0%BD%D0%BA%D0%BE%D0%B2">Страница на уютненьком Луркоморье(Нужна виртуальная сеть)</a></li>
+            </ul>
+        
+            <h3>Правила использования сайта:</h3>
+        <p>
+            1. На сайте запрещается политика, порно, шок контент и контент, запрещённый в вашей стране.<br>
+            2. Реклама на сайте допускается, если она не противоречит пункту 1.<br>
+            3. Администраторы могут удалять ваши посты, вы всегда можете узнать причину.<br>
+            4. Нарушение правил приводит к удалению постов или бану аккаунта.<br>
+            5. Админы могут забанить любой аккаунт по своему усмотрению. Если вы хотите обжаловать бан, пишите @dibof248.<br>
+
+        </p>
+
+        <h3>Мы получаем ваш Email и все указанные данные. Пароли зашифрованы.</h3>';
+    $lang['authors_text1'] = 'OneCT использует Open Source CMS, которая рассчитывает на удобное им управление, лёгкое редактирование исходного кода, создание модулей с темами и его развёртыванием на сервере'; 
+    $lang['authors_text2'] = 'На сайте мандатрами являются: '; 
+    $lang['authors_text3'] = 'OneCT использует лицензию <a href="https://github.com/OpenOneorg/OneCT/blob/main/LICENSE">BSD 3-Clause</a>, при создании форков придерживайтесь её правил.'; 
+    $lang['nickname'] = 'ФИО(Никнейм)';
+    $lang['email'] = 'Пошта';
+    $lang['pass'] = 'Парол';
+    $lang['repeat_pass'] = 'Повторите парол';
+    $lang['captcha'] = 'Мне нужны твоя одежда, сапоги и мотоцикл!';
+
+    $lang['no_user'] = 'Введите свой ник!';
+    $lang['no_email'] = 'Введите свою пошту!';
+    $lang['no_pass'] = 'Введите свой парол!';
+    $lang['no_2_pass'] = 'Повторный парол введён неверно!';
+    $lang['invalid_email'] = 'Ваша пошта не верна!';
+    $lang['full_email'] = 'Пошта уже используется!';
+    $lang['full_ip'] = 'Ваш IP-адрес уже занят другим пользователем';
+    $lang['no_captcha'] = 'Неверно введена капча!';
+    $lang['yes_reg'] = 'Вы успешно зарегистрированы';
+    $lang['server_error'] = 'Ошибка сервера';
+
+    $lang['register'] = 'Зарегистрироваться';
+    $lang['2fa_if'] = 'Код 2FA (если установлен)';
+    $lang['desc'] = 'Опесание';
+    $lang['wall'] = 'Посты';
+    $lang['post'] = 'Слить в сеть';
+    $lang['attach'] = 'Прикрепить';
+    $lang['img'] = 'Кортинко';
+    $lang['pin'] = 'Закрепленно';
+    $lang['date'] = 'd M Y в H:i';
+    $lang['by'] = 'От';
+    $lang['comments'] = 'Камменты';
+    $lang['back'] = 'Назад';
+    $lang['forward'] = 'Вперёд';
+    $lang['avatar'] = 'Юзерпик';
+    $lang['change'] = 'Изменить';
+    $lang['user_search'] = 'Поиск ванктшников';
+    $lang['name'] = 'Имя';
+    $lang['yes_post'] = 'Разрешить песать на стене';
+    $lang['lang'] = 'Йазыг';
+    $lang['style'] = 'Тема';
+    $lang['can'] = 'Можна';
+    $lang['cant'] = 'Низя';
+    $lang['disable_2fa'] = 'Отключить 2FA';
+    $lang['enable_2fa'] = 'Включить 2FA';
+    $lang['change_pass'] = 'Изменить парол';
+    $lang['old_pass'] = 'Старый парол';
+    $lang['new_pass'] = 'Новый парол';
+    $lang['pass_no'] = 'Неверный парол!';
+    $lang['old_pass_no'] = 'Неправильно введён старый парол!';
+    $lang['2_pass_no'] = 'Второй парол неверный!';
+    $lang['pass_empty'] = 'парол пустой!';
+    $lang['2fa_text'] = 'Для того чтобы отсканировать этот QR-код для включения 2FA на вашем аккаунте, нужно приложение Google Authenticator (<a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2">Android</a>). Также можно воспользоваться расширением <a href="https://chromewebstore.google.com/detail/bhghoamapcdpbohphigoooaddinpkbai">для браузеров на основе Chormium</a> или для <a href="https://addons.mozilla.org/en-US/firefox/addon/auth-helper/">Firefox</a>';
+    $lang['your_secret'] = 'Ваш секрет';
+    $lang['code'] = 'Код';
+    $lang['no_code'] = 'Неверный код!';
+    $lang['bind'] = 'Привязать';
+    $lang['unbind'] = 'Отвязать';
+    $lang['results'] = 'результатаф';
+    $lang['activation'] = 'Активация аккаунта';
+    $lang['activation_text'] = 'На указанную пошту была отправлена ссылка для активации аккаунта. Не забудьте проверить папку "спам".';
+    $lang['left1'] = ' Подождите ';
+    $lang['left2'] = ' секунд';
+    $lang['markdown_support'] = ''; // Данная функция имеет поддержку синтаксиса <a href="https://www.markdownguide.org/basic-syntax/">markdown</a>
+
+    // Footer
+
+    $lang['terms'] = 'Правила юзанья';
+    $lang['authors'] = 'Аффтары';
+
+    // API
+
+    $lang['api']['invalid_method'] = 'Неверный метод.';
+    $lang['api']['bad_login'] = 'Неверный логин или парол.';
+    $lang['api']['account_ban'] = 'Ваш аккаунт был забанен.';
+    $lang['api']['bad_2fa'] = 'Неверный код 2FA.';
+    $lang['api']['bad_token'] = 'Неверный токен.';
+    $lang['api']['no_text'] = 'Нет текста.';
+    $lang['api']['no_text_or_image'] = 'Нет текста или пикчи.';
+    $lang['api']['no_post_id'] = 'Не указан ID поста.';
+    $lang['api']['antispam'] = 'Сработала система антиспама.';
+    $lang['api']['no_user_id'] = 'Не указан ID пользователя.';
+    $lang['api']['no_group_id'] = 'Не указан ID группы.';
+    $lang['api']['not_found'] = 'Не найдено.';
+    $lang['api']['server_error'] = 'Ошибка сервера.';
+    $lang['api']['image_not_found'] = 'Пикча не найдена.';
+    $lang['api']['bad_image'] = 'Неверные параметры пикчи.';
+    $lang['api']['wall_access_denied'] = 'Доступ запрещён.';
+    $lang['api']['acc_not_act'] = 'Аккаунт не активирован!';
+    $lang['api']['long_text'] = 'Недопустимая длина текста!';
+?>
